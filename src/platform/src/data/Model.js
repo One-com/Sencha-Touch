5fc898d3786f26d9967c2b3a3c17af45fe86dbcd
Peter Müller <munter@fumle.dk>
2011-03-18 13:54:07 +0100

    Added deep validation of inner associations

--- src/platform/src/data/Model.js
+++ src/platform/src/data/Model.js
@@ -281,6 +281,13 @@ Ext.data.Model = Ext.extend(Ext.util.Stateful, {
         var errors      = new Ext.data.Errors(),
             validations = this.validations,
             validators  = Ext.data.validations,
+            inners      = this.inners,
+            innerFn     = function (record) {
+                var errs = record.validate(deep);
+                if (!errs.isValid()) {
+                    errors.addAll(errs.items);
+                }
+            },
             length, validation, field, valid, type, i;
 
         if (validations) {
@@ -300,7 +307,13 @@ Ext.data.Model = Ext.extend(Ext.util.Stateful, {
                 }
             }
         }
-        
+
+        Ext.iterate(this.associations.map, function (key, value, scope) {
+            if (value.inner === true) {
+                this[key]().each(innerFn);
+            }
+        });
+
         return errors;
     },
     
